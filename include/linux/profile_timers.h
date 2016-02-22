454cbbb2e3dde54a065f9bbbf1ccb442e7f0cf25
Russell Joyce <russell.joyce@york.ac.uk>
2016-02-22 18:23:15 +0000

    Increased number of timers from 6 to 12

--- include/linux/profile_timers.h
+++ include/linux/profile_timers.h
@@ -6,29 +6,47 @@
 extern void *profile_timers_base;
 
 #define PROF_TIMERS_BASE 0x50000000
-#define PROF_TIMERS_LENGTH 12288
+#define PROF_TIMERS_LENGTH 0x6000
 
 #define PROF_TIMER_START_COMM 0x00000080
 #define PROF_TIMER_STOP_COMM 0x00000000
 
-#define PROF_TIMER0 0x0000
-#define PROF_TLR0 0x0004
-#define PROF_TCR0 0x0008
-#define PROF_TIMER1 0x0010
-#define PROF_TLR1 0x0014
-#define PROF_TCR1 0x0018
-#define PROF_TIMER2 0x1000
-#define PROF_TLR2 0x1004
-#define PROF_TCR2 0x1008
-#define PROF_TIMER3 0x1010
-#define PROF_TLR3 0x1014
-#define PROF_TCR3 0x1018
-#define PROF_TIMER4 0x2000
-#define PROF_TLR4 0x2004
-#define PROF_TCR4 0x2008
-#define PROF_TIMER5 0x2010
-#define PROF_TLR5 0x2014
-#define PROF_TCR5 0x2018
+#define PROF_TIMER00 0x0000
+#define PROF_TLR00   0x0004
+#define PROF_TCR00   0x0008
+#define PROF_TIMER01 0x0010
+#define PROF_TLR01   0x0014
+#define PROF_TCR01   0x0018
+#define PROF_TIMER02 0x1000
+#define PROF_TLR02   0x1004
+#define PROF_TCR02   0x1008
+#define PROF_TIMER03 0x1010
+#define PROF_TLR03   0x1014
+#define PROF_TCR03   0x1018
+#define PROF_TIMER04 0x2000
+#define PROF_TLR04   0x2004
+#define PROF_TCR04   0x2008
+#define PROF_TIMER05 0x2010
+#define PROF_TLR05   0x2014
+#define PROF_TCR05   0x2018
+#define PROF_TIMER06 0x3000
+#define PROF_TLR06   0x3004
+#define PROF_TCR06   0x3008
+#define PROF_TIMER07 0x3010
+#define PROF_TLR07   0x3014
+#define PROF_TCR07   0x3018
+#define PROF_TIMER08 0x4000
+#define PROF_TLR08   0x4004
+#define PROF_TCR08   0x4008
+#define PROF_TIMER09 0x4010
+#define PROF_TLR09   0x4014
+#define PROF_TCR09   0x4018
+#define PROF_TIMER10 0x5000
+#define PROF_TLR10   0x5004
+#define PROF_TCR10   0x5008
+#define PROF_TIMER11 0x5010
+#define PROF_TLR11   0x5014
+#define PROF_TCR11   0x5018
 
 void init_profile_timers(void);
 
